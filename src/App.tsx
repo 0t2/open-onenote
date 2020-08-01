560c125848cc018c2635a3d52ad0a2c8fde18f2f
Jesse Chen <0t2jesse@gmail.com>
2020-08-01 21:29:42 +0800

    Add redirect and extract onenote url functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Switch, Route, useLocation } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [redirectUrl, setRedirectUrl] = useState("");
+  let query = useQuery();
+  useEffect(() => {
+    const oneNoteUrl = query.get("uri");
+    if (oneNoteUrl) {
+      window.open(oneNoteUrl, "onenote");
+    }
+  }, [query]);
+
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let src = event.target.value;
+    let regexp = /(onenote:.+)/;
+    let result = src.match(regexp);
+    let redirectUrl = "";
+    if (result?.[1]) {
+      redirectUrl = `${window.location.origin}?uri=${result[1]}`;
+    }
+    navigator.clipboard.writeText(redirectUrl);
+    setRedirectUrl(redirectUrl);
+    setUrl(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <div className="App">
+          <header className="App-header">
+            <textarea value={url} onChange={onChange} cols={100} rows={10} />
+            <span>{redirectUrl}</span>
+          </header>
+        </div>
+      </Route>
+    </Switch>
   );
 }
 
